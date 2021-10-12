b070232d930c70460186c342626467df5c4d0f6a
Henry Fortun <henryfortun2@gmail.com>
2021-10-11 20:13:26 -0500

    extra

---
+++ 4-RPG/Assets/Dumb/ParallaxScrolling.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] private bool repeatX;
+    [SerializeField] private bool repeatY;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+    }
+
+    // follows the camera, multiplied by the ParallaxEffectMultiplier to set a specific rate of movement
+
+    private void Update()
+    {
+            Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+            transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+            lastCameraPosition = cameraTransform.position;
+
+        // Repeats sprite infitnitely when camera reaches sprite edge. Eanble by setting repeatX or repeatY to true
+
+        if (repeatX)
+        {
+            if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+            {
+                float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+                transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+            }
+        }
+        if (repeatY)
+        {
+            if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+            {
+                float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+                transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+            }
+        }
+    }
+}
